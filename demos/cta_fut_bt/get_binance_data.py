f1955f4fdbb18839e107eb16421508ea4a263479
xiekunyan-flow <xkyfat@outlook.com>
2023-07-25 23:42:46 +0800

    获取binance 1min数据并转换成wondertrader可用

--- demos/cta_fut_bt/get_binance_data.py
+++ demos/cta_fut_bt/get_binance_data.py
@@ -1,7 +1,9 @@
 import config, csv
 import calendar
 import wget
+import os
 from binance.client import Client
+import datetime
 
 client = Client(config.API_KEY, config.API_SECRET)
 
@@ -10,12 +12,50 @@ cmonth = 1
 nyear = 2023
 nmonth : 4
 
-url = 'https://data.binance.vision/data/spot/monthly/trades/BTCUSDT/BTCUSDT-aggTrades-2023-01.zip'
-url = 'https://data.binance.vision/data/spot/monthly/trades/BNBUSDT/BNBUSDT-trades-2019-01.zip'
-url = 'https://data.binance.vision/data/spot/monthly/aggTrades/BNBUSDT/BNBUSDT-aggTrades-2019-01.zip'
-url = 'https://data.binance.vision/data/spot/monthly/aggTrades/BNBUSDT/BNBUSDT-aggTrades-2020-01.zip'
-file_name = wget.download(url)
-print(file_name)
+def timestamp_to_formatted_string(timestamp):
+    # 将时间戳转换为datetime对象
+    dt_object = datetime.datetime.fromtimestamp(timestamp)
+
+    # 将datetime对象格式化为所需的字符串
+    formatted_string = dt_object.strftime("%Y/%m/%d,%H:%M:%S")
+    date_part = formatted_string[:10]  # 提取前10个字符，即年月日部分
+    time_part = formatted_string[11:]  # 提取从第11个字符到末尾的部分，即小时分钟秒部分
+
+    return date_part,time_part
+
+# print(timestamp_to_formatted_string(1546300806260/1000))
+
+def get_data(symbol, byear, bmonth, bday, eyear, emonth, eday, period):
+    filename = symbol + '-' + str(byear) + str(bmonth).zfill(2) + str(bday).zfill(2) + '-' + str(eyear) + str(emonth).zfill(2) + str(eday).zfill(2) + '-' + period + '.csv'
+    print("csv is:", filename)
+
+    file_path = os.path.dirname(__file__)
+    os.chdir(file_path)
+
+    if not os.path.exists(os.path.join(file_path, 'cand')):
+        os.makedirs(os.path.join(file_path, 'cand'))
+
+    with open(os.path.join('cand',filename), 'w', newline='') as csvfile:
+        candlestick_writer = candlestick_writer = csv.writer(csvfile)
+
+        stDate = str(bday).zfill(2) + " " + calendar.month_abbr[bmonth] + ", " + str(byear)
+        endDate = str(eday).zfill(2) + " " + calendar.month_abbr[emonth] + ", " + str(eyear)
+        print("start time is:", stDate, " end time is:", endDate)
+        candlesticks = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, stDate, endDate)
+
+        print(len(candlesticks))
+
+        candlestick_writer.writerow(["<Date>", "<Time>", "<Open>", "<High>", "<Low>", "<Close>", "<Volume>"])
+        for candlestick in candlesticks:
+            date,time = timestamp_to_formatted_string(candlestick[0] / 1000)
+            open_price = candlestick[1]
+            high_price = candlestick[2]
+            low_price = candlestick[3]
+            close_price = candlestick[4]
+            volume = candlestick[5]
+            candlestick_writer.writerow([date,time, open_price, high_price, low_price, close_price, volume])
+
+        csvfile.close()
 
 # while cyear >= 2017:
 #     if cmonth + 3 > 12:
@@ -24,30 +64,20 @@ print(file_name)
 #     else :
 #         nyear = cyear
 #         nmonth = cmonth + 3
-#     tm = 'BTCUSDT-'+ str(cyear) + str(cmonth) +'01-' + str(nyear) + str(nmonth)+ '01-1min.csv'
-#     print("csv is:",tm)
-#     csvfile = open('cand/'+tm, 'w', newline='')
-#     candlestick_writer = csv.writer(csvfile, delimiter=',')
-
-#     stDate = "01 "+calendar.month_abbr[cmonth]+", "+str(cyear)
-#     endDate = "01 "+calendar.month_abbr[nmonth]+", "+str(nyear)
-#     print("start time is:",stDate," end time is:",endDate)
-#     candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, stDate, endDate)
-
-#     print(len(candlesticks))
-
-#     cnt = 0
-
-#     for candlestick in candlesticks:
-#         candlestick[0] = candlestick[0] / 1000 # divide timestamp to ignore miliseconds
-#         candlestick_writer.writerow(candlestick)
-#         cnt += 1
-#         if cnt % 100 == 0:
-#             print(str(cyear)+"'s "+str(cnt))
-
-#     csvfile.close()
+#     get_data("BTCUSDT",cyear,cmonth,1,nyear,nmonth,1,Client.KLINE_INTERVAL_1MINUTE)
+    
 #     if cmonth > 1:
 #         cmonth -= 3
 #     else :
 #         cyear -= 1
 #         cmonth = 10
+
+get_data("BTCUSDT",2023,7,1,2023,7,2,Client.KLINE_INTERVAL_1MINUTE)
+
+url = 'https://data.binance.vision/data/spot/monthly/trades/BTCUSDT/BTCUSDT-aggTrades-2023-01.zip'
+url = 'https://data.binance.vision/data/spot/monthly/trades/BNBUSDT/BNBUSDT-trades-2019-01.zip'
+url = 'https://data.binance.vision/data/spot/monthly/aggTrades/BNBUSDT/BNBUSDT-aggTrades-2019-01.zip'
+url = 'https://data.binance.vision/data/spot/monthly/aggTrades/BNBUSDT/BNBUSDT-aggTrades-2020-01.zip'
+url = 'https://data.binance.vision/data/spot/monthly/klines/BTCUSDT/1s/BTCUSDT-1s-2023-01.zip'
+# file_name = wget.download(url)
+# print(file_name)
--- goal.md
+++ goal.md
@@ -1,14 +1,14 @@
-1. 导入binance数据/融合[Backtrading-Python-Binance-Public](https://github.com/xiekunyan-flow/Backtrading-Python-Binance-Public)
-   1. 完成基础版本的尝试，接口跑通
+- [ ] 导入binance数据/融合[Backtrading-Python-Binance-Public](https://github.com/xiekunyan-flow/Backtrading-Python-Binance-Public)
+   - [ ] 完成基础版本的尝试，接口跑通
 
-   2. 研究下getbars的链路，首先尝试将k线数据可以正确回测
+   - [ ] 研究下getbars的链路，首先尝试将k线数据可以正确回测
 
-   3. 研究下读取订单数据
+   - [ ] 研究下读取订单数据
 
-   4. [待完成]获取一段时间内所有数据
-      1. 将数据整合
-2. 修复回测时资产负数仍然建仓的bug
-3. 成功编译wondertrader，生成so能为wtpy所用
-4. m1以下bar模式支持，需要修改wondertrader
-5. binance trade模式支持，需要修改wondertrader
-6. binance tick模式支持，需要修改wondertrader
+   - [x] 获取一段时间内所有数据
+      - [x] 将数据整合成wondertrader可用
+- [ ] 修复回测时资产负数仍然建仓的bug
+- [ ] 成功编译wondertrader，生成so能为wtpy所用
+- [ ] m1以下bar模式支持，需要修改wondertrader
+- [ ] binance trade模式支持，需要修改wondertrader
+- [ ] binance tick模式支持，需要修改wondertrader
